1bc2592f019f2e1e154415f50cdec9e389034635
Bieelogrom <gabrielamorim15436@gmail.com>
2026-06-13 16:35:06 -0300

    feat: criação da classe User e enum com roles

---
+++ src/main/java/gb/api/demo/entity/User.java
@@ -0,0 +1,31 @@
+package gb.api.demo.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table
+@NoArgsConstructor
+@Setter
+@Getter
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(length = 120, nullable = false)
+    private String nome;
+    @Column(length = 220, nullable = false)
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private UserRole role;
+}   
---
+++ src/main/java/gb/api/demo/entity/UserRole.java
@@ -0,0 +1,7 @@
+package gb.api.demo.entity;
+
+public enum UserRole {
+    ADMIN,
+    VET,
+    ADOPTER
+}
