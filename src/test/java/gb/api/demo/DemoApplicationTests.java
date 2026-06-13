14726bd8f882e675c815e0065a28c95c7a9723f5
Bieelogrom <gabrielamorim15436@gmail.com>
2026-06-13 15:05:35 -0300

    init: commit inicial

---
+++ src/test/java/gb/api/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package gb.api.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
